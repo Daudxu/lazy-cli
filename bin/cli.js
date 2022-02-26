05a642122e41af74fbf9fac133a48326e31b8a71
Daudxu <875126243@qq.com>
2022-02-26 13:37:12 +0800

    init

---
+++ bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const program = require('commander'); // npm i commander -S
+
+program.version('0.0.1')
+    .usage('<command> [项目名称]')
+    .command('init', 'init')
+    .parse(process.argv)
